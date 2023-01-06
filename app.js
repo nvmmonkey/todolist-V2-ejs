72f4221ed46ef1f0f2a962ed0abb67108fd18ccf
nvmmonkey <kit@github.com>
2023-01-06 15:38:15 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,109 @@
+// IMPORT ///
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose")
+
+// SETUP ///
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+mongoose.set("strictQuery",false)
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true})
+
+// DataBase SETUP ///
+const itemsSchema = {
+  name: String,
+}
+const listSchema = {
+  name: String,
+  items:[itemsSchema]
+}
+
+const Item  = mongoose.model("item", itemsSchema)
+const List = mongoose.model("list", listSchema)
+
+//DB default setting ///
+const item1 = new Item ({name: "Welcome to todolist"})
+const item2 = new Item ({name: "Hit + to add item"})
+const item3 = new Item ({name: "👈🏻 Hit checkbox to delete item "})
+const defaultItem = [item1, item2, item3]
+
+
+// GLOBAL VAR ///
+const workItems = [];
+const day = date.getDate();
+ 
+app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems){
+    if(foundItems.length === 0) {
+      Item.insertMany(defaultItem, function(err){
+        if(err){
+          console.log(err)
+        }else{
+          console.log("Item add to Database!")
+        }
+      })
+      res.redirect("/")
+    } else {
+      if(err){
+        console.log(err)
+      }else{
+        res.render("list", {listTitle: day, newListItems: foundItems})
+    }}
+  })
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if(!foundList){
+        //Not list found, create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItem
+        })
+        list.save()
+        console.log("Doesn't exist!")
+      } else {
+        //Found list, show existing list
+        console.log("Exist!")
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items})
+      }
+    }
+  })
+})
+
+app.post("/", function(req, res){
+  const itemName = req.body.newItem;
+  const item = new Item ({name: itemName})
+  item.save()
+  res.redirect("/")
+});
+
+app.post("/delete", function(req, res){
+  const checkItemId = req.body.checkbox
+  Item.findByIdAndRemove(checkItemId, function(err){
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Removed Checked Item!")
+      res.redirect("/")
+    }
+  })
+})
+
+
+
+
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
